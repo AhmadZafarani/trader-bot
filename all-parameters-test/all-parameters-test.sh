1bdbb13a08fa9b751e94466d2d701f6c5bccabb2
Ahmad <45295720+AhmadZafarani@users.noreply.github.com>
2021-08-30 19:14:22 +0430

    Update all-parameters-test.sh

--- all-parameters-test/all-parameters-test.sh
+++ all-parameters-test/all-parameters-test.sh
@@ -7,6 +7,6 @@ cat all-parameters-test/sed-commands-$1.txt | while read line;
 do
     eval $line
     python3 main.py
-    analyzed_o=$(python3 analyzeOutput/analyze.py only-print)
+    analyzed_o=$(python3 analyze-output/analyze.py only-print)
     echo $analyzed_o >> all-parameters-test/test-variance-and-expected-$1.csv
 done
